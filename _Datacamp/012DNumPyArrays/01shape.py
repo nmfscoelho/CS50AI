b1c3a6137999f0ba337b343e53dbbbfc6962e852
nmfscoelho <nmfscoelho@gmail.com>
2024-07-03 11:34:01 +0100

    Signed-off-by: nmfscoelho <nmfscoelho@gmail.com>

---
+++ _Datacamp/012DNumPyArrays/01shape.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+np_2d = np.array([[1.73, 1.68, 1.71, 1.89, 1.79],
+[65.4, 59.2, 63.6, 88.4, 68.7]]) 
+print(np_2d)
+print(np_2d.shape) # shape lista as colunas e registos
+
+
+
+np_3lst= np.array([[1.73, 1.68, 1.71, 1.89, 1.79],
+[65.4, 59.2, 63.6, 88.4, "68.7"]])
+
+print(np_3lst)
